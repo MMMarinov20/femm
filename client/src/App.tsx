5c5a31c8d2400e5f0ed39c23ca4429f736c92eb2
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-02-28 22:05:29 +0200

    Add rental route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Rental from "./Pages/Rental";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/rental" element={<Rental />} />
         </Routes>
       </Router>
     </React.Fragment>
---
+++ client/src/Pages/Rental.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/Home/Navbar/Navbar";
+import BurgerNavbar from "../Components/Home/Navbar/BurgerNavbar";
+
+const Rental = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <BurgerNavbar color={"#FFFFFF"} />
+    </React.Fragment>
+  );
+};
+
+export default Rental;
