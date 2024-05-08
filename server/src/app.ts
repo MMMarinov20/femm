b8f2bd282411bf6af9be7198faf2c64f41c53259
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-05-08 14:42:43 +0300

    Switch index and app files in the server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,43 +1,17 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
-import UserRoutes from "./routes/UserRoutes";
-import RentalsRoutes from "./routes/RentalsRoutes";
-import BookingRoutes from "./routes/BookingRoutes";
-import AuthenticationRoutes from "./routes/AuthenticationRoutes";
-import ReviewRoutes from "./routes/ReviewRoutes";
+const prisma = new PrismaClient();
 
-const app: Application = express();
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
 
-app.use(express.json());
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
+main()
+  .then(async () => {
+    await prisma.$disconnect();
   })
-);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World");
-});
-
-app.use("/api", AuthenticationRoutes);
-app.use("/api", RentalsRoutes);
-app.use("/api", UserRoutes);
-app.use("/api", BookingRoutes);
-app.use("/api", ReviewRoutes);
-
-app.listen(3000, () => console.log("Server running on port 3000!"));
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,43 @@
-import { PrismaClient } from "@prisma/client";
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
 
-const prisma = new PrismaClient();
+import UserRoutes from "./routes/UserRoutes";
+import RentalsRoutes from "./routes/RentalsRoutes";
+import BookingRoutes from "./routes/BookingRoutes";
+import AuthenticationRoutes from "./routes/AuthenticationRoutes";
+import ReviewRoutes from "./routes/ReviewRoutes";
 
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
+const app: Application = express();
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
   })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World");
+});
+
+app.use("/api", AuthenticationRoutes);
+app.use("/api", RentalsRoutes);
+app.use("/api", UserRoutes);
+app.use("/api", BookingRoutes);
+app.use("/api", ReviewRoutes);
+
+app.listen(3000, () => console.log("Server running on port 3000!"));
