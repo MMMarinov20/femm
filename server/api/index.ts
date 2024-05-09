e885eb23d90cd08591ba1986a22a88dd9da9666e
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-05-09 22:04:48 +0300

    Rename src to api in server

--- server/api/index.ts
+++ server/api/index.ts
@@ -40,4 +40,6 @@ app.use("/api", UserRoutes);
 app.use("/api", BookingRoutes);
 app.use("/api", ReviewRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000!"));
+app.listen(8080, () => console.log("Server running on port 3000!"));
+
+module.exports = app;
