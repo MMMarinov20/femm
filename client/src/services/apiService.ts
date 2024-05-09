6268311524d6354f33e2400ba7896ccd99aa04b7
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-05-09 22:35:26 +0300

    Update baseurl

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.BASE_URL;
+const BASE_URL = "https://femmbulgaria-server.vercel.app/api";
 
 export const apiService = {
   async get(path: string) {
