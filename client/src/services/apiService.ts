ac5cc9f06442a5e4cf0c1fe9f36629c0f137fd28
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-05-09 22:26:21 +0300

    Create .env for frontend

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,34 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = process.env.BASE_URL;
 
 export const apiService = {
-  // async get(path: string, token?: string) {
-  //   const response = await axios.get(`${BASE_URL}/${path}`, {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   });
-  //   return response.data;
-  // },
-  // async post(path: string, data?: any, token?: string) {
-  //   const response = await axios.post(`${BASE_URL}/${path}`, data, {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   });
-  //   return response.data;
-  // },
-  // async put(path: string, data: any) {
-  //   const response = await axios.put(`${BASE_URL}/${path}`, data);
-  //   return response.data;
-  // },
-  // async delete(path: string) {
-  //   const response = await axios.delete(`${BASE_URL}/${path}`);
-  //   return response.data;
-  // },
-
-  //fetch withCredentials
   async get(path: string) {
     const response = await axios.get(`${BASE_URL}/${path}`, {
       withCredentials: true,
