adcc19d05f87f7330c0bc945d5df8907b1845445
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-05-22 13:05:19 +0300

    Add speed insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { UserProvider } from "./context/UserContext.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <SpeedInsights />
     <UserProvider>
       <BrowserRouter>
         <App />
